330e1a725c907be7f33a063c756ffb8055c637e2
Jaden Guidangen <jaden_guidangen@yahoo.com>
2025-09-24 15:33:57 -0700

    usart_rebase

--- common/drivers/platform/stm32l4/st_usart.h
+++ common/drivers/platform/stm32l4/st_usart.h
@@ -24,8 +24,7 @@ public:
      * @param sys_clck The specific system clock frequency of hardware.
      * @param baud_rate The chosen baud rate to send and recieve data on a serial monitor.
      */
-    explicit StUsart(USART_TypeDef* base_addr,
-                     uint32_t sys_clck,
+    explicit StUsart(USART_TypeDef* base_addr, uint32_t sys_clck,
                      uint32_t baud_rate);
 
     /**
