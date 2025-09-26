cb7babf60770144d3a804f7e1291558462ea69da
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-09-26 16:45:24 -0700

    init func

--- app/usart_app/main.cpp
+++ app/usart_app/main.cpp
@@ -1,12 +1,9 @@
-#include <stdio.h>
 #include "usart_app_bsp.h"
 
 int main(int argc, char** argv)
 {
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-    RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
 
-    uint8_t data[10] = {"testin8\n\r"};
+    uint8_t data[] = {"testin8\n\r"};
     Usart& usart = BSP_Init(USART2);
 
     usart.send_tx(data, sizeof(data));
