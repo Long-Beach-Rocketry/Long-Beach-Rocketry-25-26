888e490742d046bc54c4f5fd8904092eba4e92fd
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-09-26 16:45:24 -0700

    init func

--- common/drivers/platform/stm32l4/st_usart.cpp
+++ common/drivers/platform/stm32l4/st_usart.cpp
@@ -12,12 +12,27 @@ void StUsart::receive_rx(volatile uint8_t* data, size_t size)
 void StUsart::send_tx(uint8_t* data, size_t size)
 {
     size_t count = 0;
-    while (1)
+    while (count < size)
     {
         instance->TDR = data[count++];
     }
 }
 
-void StUsart::fake_func()
+void StUsart::init(USART_TypeDef* instance)
 {
+    StUsart::instance = instance;
+
+    // StUsart::rx_pin = rx;
+    // StUsart::tx_pin = tx;
+    // rx_pin->configure();
+    // tx_pin->configure();
+
+    instance->CR1 &= ~USART_CR1_UE;
+    instance->CR1 |= USART_CR1_RE;
+    instance->CR1 |= USART_CR1_TE;
+    instance->CR1 &= ~USART_CR1_M0;
+    instance->CR1 &= ~USART_CR1_M1;
+    instance->BRR = (((uartdiv / 16) << USART_BRR_DIV_MANTISSA_Pos) |
+                     ((uartdiv % 16) << USART_BRR_DIV_FRACTION_Pos));
+    instance->CR1 |= USART_CR1_UE;
 }
--- common/drivers/platform/stm32l4/st_usart.h
+++ common/drivers/platform/stm32l4/st_usart.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // #include "gpio.h"
+#include <stdbool.h>
 #include <cstddef>
 #include "stm32l476xx.h"
 #include "usart.h"
@@ -16,27 +17,14 @@ private:
 public:
     void send_tx(uint8_t* data, size_t size) override;
     void receive_rx(volatile uint8_t* data, size_t size) override;
-    void fake_func();
+    void init(USART_TypeDef* instance);
 
     StUsart()
     {
     }
-    StUsart(USART_TypeDef* instance, int sys_clck, int baud_rate)
+    StUsart(int sys_clck, int baud_rate)
     {
-        StUsart::instance = instance;
-        // StUsart::rx_pin = rx;
-        // StUsart::tx_pin = tx;
-        // rx_pin->configure();
-        // tx_pin->configure();
 
         uartdiv = sys_clck / baud_rate;
-        instance->CR1 &= ~USART_CR1_UE;
-        instance->CR1 |= USART_CR1_RE;
-        instance->CR1 |= USART_CR1_TE;
-        instance->CR1 &= ~USART_CR1_M0;
-        instance->CR1 &= ~USART_CR1_M1;
-        instance->BRR = (((uartdiv / 16) << USART_BRR_DIV_MANTISSA_Pos) |
-                         ((uartdiv % 16) << USART_BRR_DIV_FRACTION_Pos));
-        instance->CR1 |= USART_CR1_UE;
     }
 };
