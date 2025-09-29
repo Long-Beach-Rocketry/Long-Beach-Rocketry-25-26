155d752348317797210f3ee27091f2829cf9add6
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-09-28 21:56:28 -0700

    usart transmit

--- app/usart_app/BSP_L476/usart_app_bsp.cpp
+++ app/usart_app/BSP_L476/usart_app_bsp.cpp
@@ -1,24 +1,25 @@
 #include "usart_app_bsp.h"
+#include <stdint.h>
 // static st_gpio rx_pin(GPIOA, 5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL,
 //                       GPIO_SPEED_FREQ_LOW, 0);
 // static st_gpio tx_pin(GPIOA, 6, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL,
 //                       GPIO_SPEED_FREQ_LOW, 0);
 
-StUsart stusart(80000000, 115200);
+StUsart stusart{(uint32_t)4000000, (uint32_t)115200};
+
+Stml4::StGpioSettings config{Stml4::GpioMode::ALT_FUNC,
+                             Stml4::GpioOtype::PUSH_PULL,
+                             Stml4::GpioOspeed::LOW,
+                             Stml4::GpioPupd::NO_PULL,
+                             0x7};
+
+Stml4::HwGpio tx_pin{config, (uint8_t)2, GPIOA};
 
 Usart& BSP_Init(USART_TypeDef* base_addr)
 {
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
     RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
 
-    stusart.init(base_addr);
+    stusart.init(base_addr, &tx_pin);
     return stusart;
 }
-
-// Gpio& BSP_GPIO_Init(GPIO_TypeDef* base_addr, uint8_t pin, uint32_t mode,
-//                     uint32_t pull, uint32_t speed, uint32_t af)
-// {
-//     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-
-//     return StGpio;
-// }
--- app/usart_app/main.cpp
+++ app/usart_app/main.cpp
@@ -2,11 +2,15 @@
 
 int main(int argc, char** argv)
 {
+    RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
+    uint8_t data[] = {"testin8"};
+    // Usart& usart = BSP_Init(USART2);
 
-    uint8_t data[] = {"testin8\n\r"};
-    Usart& usart = BSP_Init(USART2);
+    // usart.send_tx(data, sizeof(data));
 
-    usart.send_tx(data, sizeof(data));
+    BSP_Init(USART2).send_tx(data, sizeof(data));
+
+    while (1);
 
     return 0;
 }
