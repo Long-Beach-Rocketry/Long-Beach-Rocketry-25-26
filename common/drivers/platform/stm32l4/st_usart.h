e9c7dc5c13546ba571f424c9d3b211f0c83336cb
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-09-28 21:56:28 -0700

    usart transmit

--- common/drivers/platform/stm32l4/st_usart.h
+++ common/drivers/platform/stm32l4/st_usart.h
@@ -5,8 +5,11 @@
 #pragma once
 
 #include <cstddef>
+<<<<<<< HEAD
 #include <span>
 #include <string>
+=======
+>>>>>>> 6b9398d (usart transmit)
 #include "gpio.h"
 #include "stm32l476xx.h"
 #include "usart.h"
@@ -14,6 +17,7 @@
 class StUsart : public Usart
 {
 private:
+<<<<<<< HEAD
     USART_TypeDef* base_addr;
     uint16_t uartdiv;
 
@@ -48,4 +52,23 @@ public:
      * @param instannce The USART base_addr that is to be currently used.
      */
     void init();
+=======
+    // Gpio* rx_pin;
+    LBR::Gpio* tx_pin;
+    USART_TypeDef* instance;
+    uint16_t uartdiv;
+
+public:
+    void send_tx(const uint8_t* data, size_t size) override;
+    void receive_rx(volatile uint8_t* data, size_t size) override;
+    void init(USART_TypeDef* instance, LBR::Gpio* tx);
+
+    StUsart()
+    {
+    }
+    StUsart(uint32_t sys_clck, uint32_t baud_rate)
+    {
+        uartdiv = sys_clck / baud_rate;
+    }
+>>>>>>> 6b9398d (usart transmit)
 };
