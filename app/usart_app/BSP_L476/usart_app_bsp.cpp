1f0d209b5a6a1a2441160e854ba8b10561a0fe1d
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-09-30 18:50:24 -0700

    working usart

--- app/usart_app/BSP_L476/usart_app_bsp.cpp
+++ app/usart_app/BSP_L476/usart_app_bsp.cpp
@@ -1,25 +1,29 @@
 #include "usart_app_bsp.h"
 #include <stdint.h>
-// static st_gpio rx_pin(GPIOA, 5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL,
-//                       GPIO_SPEED_FREQ_LOW, 0);
-// static st_gpio tx_pin(GPIOA, 6, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL,
-//                       GPIO_SPEED_FREQ_LOW, 0);
 
 StUsart stusart{(uint32_t)4000000, (uint32_t)115200};
 
-Stml4::StGpioSettings config{Stml4::GpioMode::ALT_FUNC,
-                             Stml4::GpioOtype::PUSH_PULL,
-                             Stml4::GpioOspeed::LOW,
-                             Stml4::GpioPupd::NO_PULL,
-                             0x7};
+Stml4::StGpioSettings config_tx{Stml4::GpioMode::ALT_FUNC,
+                                Stml4::GpioOtype::PUSH_PULL,
+                                Stml4::GpioOspeed::LOW,
+                                Stml4::GpioPupd::NO_PULL,
+                                0x7};
 
-Stml4::HwGpio tx_pin{config, (uint8_t)2, GPIOA};
+Stml4::StGpioSettings config_rx{Stml4::GpioMode::ALT_FUNC,
+                                Stml4::GpioOtype::PUSH_PULL,
+                                Stml4::GpioOspeed::LOW,
+                                Stml4::GpioPupd::NO_PULL,
+                                0x7};
+
+Stml4::HwGpio rx_pin(config_rx, (uint8_t)3, GPIOA);
+
+Stml4::HwGpio tx_pin{config_tx, (uint8_t)2, GPIOA};
 
 Usart& BSP_Init(USART_TypeDef* base_addr)
 {
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
     RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
 
-    stusart.init(base_addr, &tx_pin);
+    stusart.init(base_addr, &tx_pin, &rx_pin);
     return stusart;
 }
--- common/drivers/bus/usart.h
+++ common/drivers/bus/usart.h
@@ -4,5 +4,5 @@ class Usart
 {
 public:
     virtual void send_tx(const uint8_t* data, size_t size) = 0;
-    virtual void receive_rx(volatile uint8_t* data, size_t size) = 0;
+    virtual bool receive_rx(volatile uint8_t* data, size_t size) = 0;
 };
