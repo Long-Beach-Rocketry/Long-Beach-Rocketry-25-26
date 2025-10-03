7b3e02541400527577e8382d15d7f23d09274334
Bex Saw <bex.bcc171@gmail.com>
2025-10-02 17:10:45 -0700

    Add Header Files

---
+++ common/drivers/platform/stm32l4/st_spi_factory.cc
@@ -0,0 +1,58 @@
+/**
+ * @file st_spi_factory.cc
+ * @author Bex
+ * @brief Factory for creating valid SPI objects with GPIO settings
+ * @version 0.1
+ * @date 2025-10-05
+ */
+
+ #include "st_spi_factory.h"
+
+ namespace LBR {
+    namespace Stml4 {
+        /**
+         * @brief Create an SPI object with given instance, GPIO config, and SPI Settings
+         * - Configures SCK, MOSO, MISO pins with alternate function
+         * - Applies user-provided SPI settings
+         */
+        HwSpi CreateSpi(SPI_TypeDef* spi_instance, const SpiGpioConfig& gpio_config, const StSpiSettings& spi_settings) {
+            
+            // Construct SPI object with instance + settings
+            HwSpi spi(spi_instance, spi_settings);
+
+            // Initialize GPIO pins for SPI
+            HwGpio sck_pin(gpio_config.sck_settings.port, gpio_config.sck_settings.pin_num);
+            sck_pin.Init(CreateSpiPinSettings(spi_settings.af));
+            HwGpio miso_pin(gpio_config.miso_settings.port, gpio_config.miso_settings.pin_num);
+            miso_pin.Init(CreateSpiPinSettings(spi_settings.af));
+            HwGpio mosi_pin(gpio_config.mosi_settings.port, gpio_config.mosi_settings.pin_num);
+            mosi_pin.Init(CreateSpiPinSettings(spi_settings.af));
+
+            return spi;
+        }
+
+    /** 
+     * @brief Validate the SPI object
+     * @return true if initialized successfully, false otherwise
+     */
+    bool ValidateSpi(HwSpi& spi) {
+        return(spi.Init() == SpiStatus::OK);
+    }
+
+    /**
+     * @brief High-level factory function to get a validated SPI object
+     * - Creates SPI object with GPIO config and settings
+     * - Validates initialization
+     * - Returns SPI object if valid, otherwise a default invalid SPI object
+     */
+    HwSpi GetSpi(SPI_TypeDef* spi_instance, const SpiGpioConfig& gpio_config, const StSpiSettings& spi_settings) {
+        HwSpi spi = CreateSpi(spi_instance, gpio_config, spi_settings);
+        if (ValidateSpi(spi)) {
+            return HwSpi();
+        } else {
+            // Return a default invalid SPI object
+            return HwSpi(nullptr, StSpiSettings{});
+        }
+}
+}
+}
