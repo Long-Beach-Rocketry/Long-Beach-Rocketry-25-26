49ca84ac185c7489ff0752f1147c758bbf8498dd
Phoebe H <pbho48985219@gmail.com>
2025-10-02 20:29:35 -0700

    added spi stuff

---
+++ common/drivers/platform/stm32l4/st_spi.h
@@ -0,0 +1,85 @@
+/**
+ * @author Phoebe Ho
+ * @date 10/2/25    
+ */
+//spi interface
+//INIT Function
+/*
+initializes SPI Driver (internal data structures or pointers and specific driver implementations)
+pin configurations: MOSI, MISO, SCK, CS/SS
+clock configurations: enable peripheral clocks for SPI module and GPIO Ports (double check GPIO code)
+Mode settings: baud rate, data order (MSB AND LSB), bits per transfer, and SPI mode 
+spi peripheral : SPI1, SPI2, etc.
+*/
+#pragma once
+
+#include "stm32l476xx.h"
+#define ST_GPIO_MAX_PINS 16 //defining amt of pins for stm32
+
+namespace LBR{
+namespace STM32{
+    
+enum class SpiPinMode : std::uint8_t{ //defining modes of the pin 
+    Mode1 = 0,
+    Mode2,
+    Mode3,
+    Mode4
+        };
+enum class SpiBitOrder : std::uint8_t{
+    MSB_First=0,
+    LSB_First
+};
+enum class SpiError: std::uint8_t{
+    //need to add
+};
+enum class SpiBaudRate: std::uint8_t{ // std::uint8_t is to ensure it's an unassigned integer of 8 bits
+    Baud2 =0,
+    Baud4,
+    Baud8,
+    Baud16,
+    Baud32,
+    Baud64,
+    Baud128,
+    Baud256
+};
+
+enum class SpiEnable : std::uint8_t{
+    Enable = 0,
+    Disable
+};
+class HwSpi
+{ //pins should already be configured
+public: 
+    explicit HwSpi(SPI_TypeDef* spi, Pin mosi, Pin miso, Pin clk); //passing SPI1, SPI2, etc
+    //constructor declaration for a class: HwSpi ^
+    bool spi_init();  
+   //init function takes in no arguments --telling compiler it's meant to override a virtual function from a base 
+    //class and if it's not found, it will generate an error (virtual so it can be overriden by derived classes)
+    /*
+    derived class: the new class created to inherit the parent class 
+    ex: class dog : public Animal (dog is a derived class, so if you call Animal* a = new dog(), you
+    actually call the functions of Animal instead, but it can override virtual methods if they're the same name) 
+    */
+   
+     
+    
+
+private: //only accessible to the member functions of HwSpi (spi's own methods) (if i want initClocks(), need to call spi_init()
+    //defining everything in the constructor
+    SPI_TypeDef* const spi_; //spi_ underscore is naming convention for private members
+    //^ private pointer to hardware Spi peripheral 
+    /*SPI_TypeDef
+    - C struct defined in stm32l476xx.h
+    - so we can use SPI1->CR1 |= SPI_CR1_SPE instead of mapping each one individually
+    */
+    Pin mosi;
+    Pin miso;
+    Pin clk;
+
+    void enableSPI(SpiEnable enable); //using enableSPI class with SpiEnable parameters
+    void setMode(SpiPinMode mode); 
+        
+    };
+}; //semicolon after classes
+}
+}
