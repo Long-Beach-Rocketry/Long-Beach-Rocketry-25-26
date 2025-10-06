9bed0019369939482a9a8e61e9b0fb8a2c55b148
Theodore Malaska <malaskatj@gmail.com>
2025-10-05 23:36:53 -0700

    Tm gpio clean up (#26)
    * cleaned up
    
    * done
    
    * done
    
    * fixed minor error
    
    * fixed minor error

--- app/blink/bsp_l476/l476_board.cc
+++ app/blink/bsp_l476/l476_board.cc
@@ -5,24 +5,33 @@
 
 namespace LBR
 {
-namespace Board
-{
-Stml4::StGpioSettings led_cfg{
+Stml4::StGpioSettings led_settings{
     Stml4::GpioMode::GPOUT, Stml4::GpioOtype::PUSH_PULL, Stml4::GpioOspeed::LOW,
     Stml4::GpioPupd::NO_PULL, 0};
-Stml4::HwGpio led{led_cfg, static_cast<uint8_t>(5), GPIOA};
 
-bool init()
+Stml4::StGpioParams led_params{
+    led_settings,
+    static_cast<uint8_t>(5),
+    GPIOA,
+};
+
+Stml4::HwGpio led{led_params};
+
+Board board{.led = led};
+
+bool board_init()
 {
+    bool return_val = true;
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 
-    led.init();
-    return true;
+    return_val = return_val && led.init();
+
+    return return_val;
 }
 
-Gpio& get_led()
+Board& get_board(void)
 {
-    return led;
+    return board;
 }
-}  // namespace Board
+
 }  // namespace LBR
