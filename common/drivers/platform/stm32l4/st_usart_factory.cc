47553dc0a45407ce986fd6cc16b8afccced47660
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-10-07 17:57:08 -0700

    func

--- common/drivers/platform/stm32l4/st_usart_factory.cc
+++ common/drivers/platform/stm32l4/st_usart_factory.cc
@@ -7,9 +7,11 @@ StUsart& CreateUsart(uint32_t sys_clck, uint32_t baud_rate,
                      uint8_t tx_pin, GPIO_TypeDef* rx_base_addr,
                      GPIO_TypeDef* tx_base_addr)
 {
+    const LBR::Stml4::StGpioParams& rx_params{rx_config, rx_pin, rx_base_addr};
+    const LBR::Stml4::StGpioParams& tx_params{tx_config, tx_pin, tx_base_addr};
     static StUsart st_usart(base_addr, sys_clck, baud_rate);
-    LBR::Stml4::HwGpio rx_gpio(rx_config, rx_pin, rx_base_addr);
-    LBR::Stml4::HwGpio tx_gpio(tx_config, tx_pin, tx_base_addr);
+    LBR::Stml4::HwGpio rx_gpio(rx_params);
+    LBR::Stml4::HwGpio tx_gpio(tx_params);
 
     st_usart.init();
     rx_gpio.init();
