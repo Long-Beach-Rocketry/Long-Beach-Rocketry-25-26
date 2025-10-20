e2d0d0a9241d2dd83c1842f595c1dae9e0657fb4
yshiblanco <yshi.blanco@gmail.com>
2025-10-20 11:52:53 -0700

    updated i2c module: removed GiveI2c method and changed overall functionality

--- common/drivers/platform/stm32l4/st_i2c_module.h
+++ common/drivers/platform/stm32l4/st_i2c_module.h
@@ -21,30 +21,29 @@ class I2cModule
 public:
     /**
      * @brief I2cModule constructor
-     */
-    I2cModule();
-
-    /**
-     * @brief Create instances of necessary peripherals and initialize them
      * 
      * @param base_addr base address of I2C peripheral
      * @param timingr timing settings
      * @param sda_pin struct containing settings, pin number, and port address for SDA pin
      * @param scl_pin struct containing settings, pin number, and port address for SCL pin
-     * @return true if success, false otherwise
      */
-    bool GiveI2c(I2C_TypeDef* base_addr, uint32_t timingr,
-                 const StGpioParams& sda_pin, const StGpioParams& scl_pin);
+    explicit I2cModule(I2C_TypeDef* base_addr, uint32_t timingr,
+                       const StGpioParams& sda_pin,
+                       const StGpioParams& scl_pin);
+    ;
 
     /**
      * @brief Creates HwI2c instance
      * 
-     * @return HwI2c instance
+     * @return reference to HwI2c instance
      */
-    HwI2c* CreateI2c();
+    HwI2c& CreateI2c();
 
 private:
-    HwI2c* _i2c;
+    I2C_TypeDef* _base_addr;
+    uint32_t _timingr;
+    const StGpioParams& _sda_pin;
+    const StGpioParams& _scl_pin;
 };
 }  // namespace Stml4
 }  // namespace LBR
