fd25f83e3e1309bd50a8269d57b88638d7f55f09
Bex Saw <bex.bcc171@gmail.com>
2025-10-22 12:44:41 -0700

    IMU Interface & Some Implementation

---
+++ common/sensors/imu.h
@@ -0,0 +1,35 @@
+/**
+* @file imu.h
+* @brief Inertial Measurement Unit (IMU) sensor interface
+* @author Bex Saw
+* @date 2025-10-22
+* @note This file contains the interface for the IMU sensor, including initialization, reading, and calibration functions.
+*/
+
+#pragma once
+#include <cstdint>
+
+namespace LBR 
+{
+class Imu
+{
+public:
+    /**
+    * @brief - Initializes the IMU hardware
+    *        - Research IMU data (poll or DMA)
+    *        - Get Acceleration in m/s^2
+    *        - Get Angular velocity in degrees/s
+    *        - Get Euler angles in degrees
+    *        - Calibrate the IMU
+    *        - Virtual destructor
+    *
+    */
+    virtual void Init() = 0;
+    virtual void Update() = 0;
+    virtual void GetAcceleration(float& x, float& y, float& z) = 0;
+    virtual void GetGyroscope(float& x, float& y, float& z) = 0;
+    virtual void GetEulerAngles(float& roll, float& pitch, float& yaw) = 0;
+    virtual void Calibrate() = 0;
+    virtual ~Imu() = default;
+}
+} // namespace LBR
