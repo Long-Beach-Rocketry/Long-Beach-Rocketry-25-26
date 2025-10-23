f524086263a4408b4f8ff2440ee9061afc3b40d8
Jaden803 <131480961+Jaden803@users.noreply.github.com>
2025-10-23 08:54:57 -0700

    function implementations

--- common/drivers/platform/bno055/bno055_imu.cc
+++ common/drivers/platform/bno055/bno055_imu.cc
@@ -1,5 +1,33 @@
 #include "bno055_imu.h"
 
+BNO_IMU::BNO_IMU(HwI2c& i2c) : i2c(i2c) {};
+
+void condig_imu()
+{
+    BNO_IMU::i2c.init();
+    //setting up sensor fusion
+    //not actuall function
+    i2c_write(OPR_NDOF_MODE, OPR_MODE_ADDR);
+}
+
 void get_data()
 {
+    uint8_t accel_data_x[2];  //MSB and LSB
+    uint8_t accel_data_y[2];  //...
+
+    uint8_t gyro_data_x[2];  //MSB and LSB
+    uint8_t gyro_data_y[2];  //...
+
+    //not actual i2c function
+    i2c_read(accel_data_x, ACCEL_X_LSB);
+    i2c_read(accel_data_y, ACCEL_Y_LSB);
+
+    i2c_read(gyro_data_x, GRYO_X_LSB);
+    i2c_read(gyro_data_y, GRYO_Y_LSB);
+
+    BNO_IMU::accel_x = ((int16_t)(accel_data_x[1] << 8) | accel_data_x[0]);
+    BNO_IMU::accel_y = ((int16_t)(accel_data_y[1] << 8) | accel_data_y[0]);
+
+    BNO_IMU::gyro_x = ((int16_t)(gyro_data_x[1] << 8) | gyro_data_x[0]);
+    BNO_IMU::gyro_y = ((int16_t)(gyro_data_y[1] << 8) | gyro_data_y[0]);
 }
--- common/drivers/platform/bno055/bno055_imu.h
+++ common/drivers/platform/bno055/bno055_imu.h
@@ -1,11 +1,33 @@
 #pragma once
 
 #include "imu.h"
+#include "st_i2c.h"
+#include "stm32l4xx.h"
+
+#define OPR_NDOF_MODE 0b1100;
+#define DEVICE_ADDR 0x28;
+#define OPR_MODE_ADDR 0x3D;
+#define ACCEL_X_LSB 0x08;
+#define ACCEL_X_MSB 0x09;
+#define ACCEL_Y_LSB 0x0A;
+#define ACCEL_Y_MSB 0x0B;
+#define GRYO_X_LSB 0x14;
+#define GRYO_X_MSB 0x15;
+#define GRYO_Y_LSB 0x16;
+#define GRYO_Y_MSB 0x17;
 
 class BNO_IMU : public IMU
 {
+private:
+    HwI2c& i2c;
+    int16_t accel_x, accel_y, gyro_x, gyro_y;
+
 public:
+    explicit BNO_IMU(HwI2c& i2c);
+
     void get_data() override;
 
-    void calculate_data();
+    // void calculate_data();
+
+    void config_imu();
 };
