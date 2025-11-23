72c0f1a21f44fcd5eefd12eca2ba706ea3f3d24f
Kent Hong <kent.n.hong@gmail.com>
2025-11-22 18:36:37 -0800

    Implemented BusyCheck

--- common/core/periph/flash_chip/w25q.h
+++ common/core/periph/flash_chip/w25q.h
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include "spi.h"
-
+#include "gpio_cs.h"
+#include <span>
+#include <array>
 
 namespace LBR
 {
@@ -14,12 +16,12 @@ enum class StatusRegister : uint8_t {
 class W25q
 {
 public:
-    W25q(Spi& spi_);
+    W25q(Spi& spi_, GpioChipSelect& cs_);
 
     bool StatusRead(StatusRegister status_reg_num, std::span<uint8_t> rxbuf);
     void Reset();
-    bool Read(uint16_t sector, uint8_t page, std::span<uint8_t> rxbuf);
-    bool PageProgram(uint16_t sector, uint8_t page, std::span<uint8_t> txbuf);
+    bool Read(uint16_t sector, uint8_t page, uint8_t offset, std::span<uint8_t> rxbuf);
+    bool PageProgram(uint16_t sector, uint8_t page, uint8_t offset, std::span<uint8_t> txbuf, std::span<uint8_t> rxbuf);
     bool SectorErase(uint16_t sector);
     bool ChipErase();
 
@@ -30,5 +32,6 @@ public:
 private:
     bool BusyCheck();
     Spi& spi;
+    GpioChipSelect& cs;
 };
 }  // namespace LBR
