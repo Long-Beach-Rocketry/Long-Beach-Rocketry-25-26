189c755045eaac7f58f90993a8715ca1664f9d11
yshiblanco <yshi.blanco@gmail.com>
2025-11-24 21:11:17 -0800

    modified structure, added functionality for 16 bit reg addr

--- common/drivers/platform/stm32l4/st_i2c.h
+++ common/drivers/platform/stm32l4/st_i2c.h
@@ -40,33 +40,12 @@ public:
      */
     bool init();
 
-    bool mem_read(std::span<uint8_t> data, std::span<const uint8_t> reg_addr,
-                  uint8_t dev_addr) override;
-    bool mem_write(std::span<const uint8_t> data,
-                   std::span<const uint8_t> reg_addr,
-                   uint8_t dev_addr) override;
-
 private:
     I2C_TypeDef* _base_addr;
     uint32_t _timingr;
 
-    /**
-     * @brief Read raw data from an I2C bus
-     * 
-     * @param data block of memory to read data into from the bus
-     * @param dev_addr address of target device
-     * @return true if successful, false otherwise
-     */
-    bool burst_read(std::span<uint8_t> data, uint8_t dev_addr);
-
-    /**
-     * @brief Write raw data to an I2C bus
-     * 
-     * @param data block of memory to write data into the bus
-     * @param dev_addr address of target device
-     * @return true if successful, false otherwise
-     */
-    bool burst_write(std::span<const uint8_t> data, uint8_t dev_addr);
+    bool burst_read(std::span<uint8_t> data, uint8_t dev_addr) override;
+    bool burst_write(std::span<const uint8_t> data, uint8_t dev_addr) override;
 };
 }  // namespace Stml4
 }  // namespace LBR
