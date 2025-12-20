fea3ffb793d4de40855f9c0add21c82bbd7d2533
TJMalaska <malaskatj@gmail.com>
2025-12-20 14:13:45 -0800

    save

---
+++ common/drivers/time/sys_clock.h
@@ -0,0 +1,13 @@
+/**
+ * @file gpio.h
+ * @brief Gpio driver interface.
+ * @author TJ Malaska
+ * @date 9/13/2025
+ *
+ */
+
+#pragma once
+namespace LBR
+{
+    //hard code the clock set up first to test
+}
