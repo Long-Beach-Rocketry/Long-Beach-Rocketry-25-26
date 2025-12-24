9b6be2a955f91ddb8040e345059b0205af318e94
yshiblanco <yshi.blanco@gmail.com>
2025-12-24 13:00:03 -0800

    rebase with main
    added settings
    
    check error cases and minor implementation updates
    
    finish init; not tested
    
    fixed some compiler errors w init
    
    implement set_freq; not tested
    
    implemented set_duty_cycle; not tested
    
    created test application
    
    added notes and extra checks for failure
    
    fix rebase
    
    rebase yb-pwm for compile test
    
    rebase origin
    
    finished DRV8874

---
+++ app/drv8874_test/main.cc
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstdlib>
+#include "drv8874.h"
+#include "board.h"
+
+using namespace LBR;
+
+int main(int argc, char* argv[])
+{
+    bsp_init();
+    Board hw = get_board();
+    
+
+    while (1)
+    {
+        // Drive forward at 50% duty cycle
+        hw.motor.setSpeed(50);
+        hw.motor.setDirection(Drv8874::Direction::FORWARD);
+        for (volatile size_t i = 0; i < 1000000; i++);
+
+        // Enable brake then coast
+        hw.motor.enableBrake();
+        for (volatile size_t i = 0; i < 1000000; i++);
+        hw.motor.enableCoast();
+        for (volatile size_t i = 0; i < 1000000; i++);
+
+        // setSleep false (sleep mode)
+        hw.motor.setSleep(false);
+        for (volatile size_t i = 0; i < 1000000; i++);
+        
+        // Check for faults
+        if (hw.motor.checkFault()) {
+            return -1;
+        } else {
+            return 0;
+        }
+
+        // Drive backward at 75% duty cycle
+        hw.motor.setSpeed(-75);
+        hw.motor.setSleep(true); // Wake up
+        for (volatile size_t i = 0; i < 1000000; i++);
+    }
+
+    return 0;
+}
