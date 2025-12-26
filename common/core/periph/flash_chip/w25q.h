ab0a81ba5be90b39d479419322c1ac5d8acf105b
Kent Hong <kent.n.hong@gmail.com>
2025-12-26 15:26:56 -0800

    Implement StatusRegWrite, BlockErase, BlockLock, BlockUnlock, and BlockLockStatusRead functions

--- common/core/periph/flash_chip/w25q.h
+++ common/core/periph/flash_chip/w25q.h
@@ -4,12 +4,20 @@
 #include <cstring>
 #include <span>
 #include "gpio_cs.h"
+
 #include "spi.h"
 
 namespace LBR
 {
 
-enum class StatusRegister : uint8_t
+enum class StatusWrite : uint8_t
+{
+    STATUS_REGISTER_1 = 0x01u,
+    STATUS_REGISTER_2 = 0x31u,
+    STATUS_REGISTER_3 = 0x11u
+};
+
+enum class StatusRead : uint8_t
 {
     STATUS_REGISTER_1 = 0x05u,
     STATUS_REGISTER_2 = 0x35u,
@@ -21,23 +29,29 @@ class W25q
 public:
     W25q(Spi& spi_, GpioChipSelect& cs_);
 
-    bool StatusRead(StatusRegister status_reg_num, std::span<uint8_t> rxbuf);
+    void W25qInit();
+    bool StatusRegWrite(StatusWrite status_reg_num, uint8_t status_byte);
+    bool StatusRegRead(StatusRead status_reg_num, std::span<uint8_t> rxbuf);
     void Reset();
     bool Read(uint8_t block, uint8_t sector, uint8_t page, uint8_t offset,
               std::span<uint8_t> rxbuf);
     bool PageProgram(uint8_t block, uint8_t sector, uint8_t page,
                      uint8_t offset, std::span<uint8_t> txbuf,
                      std::span<uint8_t> rxbuf);
+    // TODO: Implement BlockErase()
+    bool BlockErase(uint8_t block);
     bool SectorErase(uint8_t block, uint8_t sector);
     bool ChipErase();
 
     // In case we want to erase entire chip except some blocks
-    bool SectorProtect(uint16_t sector);
-    bool BlockProtect(uint8_t block);
+    bool BlockLock(uint8_t block);
+    bool BlockUnlock(uint8_t block);
 
 private:
     bool BusyCheck();
     void WriteEnable();
+    bool BlockLockStatusRead(uint32_t block_addr,
+                             std::array<uint8_t, 1> block_lock_status);
     Spi& spi;
     GpioChipSelect& cs;
 };
