fc335f7758f75085664d0ceea8c69812703b3164
Bex Saw <bex.bcc171@gmail.com>
2026-01-10 22:52:25 -0800

    pps board hw abstraction in bsp -- first try

---
+++ app/pps_board/board.h
@@ -0,0 +1,31 @@
+/**
+ * @file board.h
+ * @brief PPS test board interface
+ * @author Bex Saw
+ * @date 10/21/2025
+ */
+
+#pragma once
+
+// When the ADC driver is written
+//#include "uart.h"
+//#include "adc.h"
+//#include "pwm.h"
+//#include encoder driver when written
+#include "gpio.h"
+
+namespace LBR
+{
+
+struct Board
+{
+    Gpio& gpio;
+    Pwm& pwm;
+    //Uart& uart;
+    //Adc& adc;
+    //Encoder& encoder;
+};
+
+bool bsp_init(void);
+Board& get_board();
+}  // namespace LBR
