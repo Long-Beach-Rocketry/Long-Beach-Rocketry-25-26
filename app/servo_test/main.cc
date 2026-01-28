d34478993825596cfb9ee695066059e3d2562947
yshiblanco <yshi.blanco@gmail.com>
2026-01-28 13:29:03 -0800

    created test application

---
+++ app/servo_test/main.cc
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include "board.h"
+#include "servo_motor.h"
+
+using namespace LBR;
+
+int main(int argc, char* argv[])
+{
+    bsp_init();
+    Board hw = get_board();
+    uint8_t angle = 0;
+
+    while (1)
+    {
+        angle += 10;
+        if (angle > 180)
+        {
+            angle = 0;
+        }
+
+        hw.servo_motor.set_angle(angle);
+
+        for (volatile size_t i = 0; i < 2000000; i++);
+    }
+
+    return 0;
+}
