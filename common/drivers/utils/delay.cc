6b168d72386e1df0f9192297c84be02a65e4c8b6
Bex Saw <bex.bcc171@gmail.com>
2026-01-28 20:42:47 -0800

    unit_test pass

--- common/drivers/utils/delay.cc
+++ common/drivers/utils/delay.cc
@@ -4,7 +4,17 @@
 namespace
 {
 volatile uint32_t g_ms_ticks = 0;
-}
+
+// ARM-specific interrupt disable/enable macros
+#ifdef __arm__
+#define DISABLE_IRQ() __asm__ volatile("cpsid i" : : : "memory")
+#define ENABLE_IRQ()  __asm__ volatile("cpsie i" : : : "memory")
+#else
+// For non-ARM (native builds), these are no-ops
+#define DISABLE_IRQ()
+#define ENABLE_IRQ()
+#endif
+}  // namespace
 
 // SysTick_Handler is defined in st_sys_clock.cc
 // Provide a way for it to increment g_ms_ticks
@@ -20,30 +30,32 @@ void DelayMs(uint32_t ms)
 {
     uint32_t start;
     // Atomically read start time
-    __asm__ volatile("cpsid i" : : : "memory");  // Disable interrupts
+    DISABLE_IRQ();
     start = g_ms_ticks;
-    __asm__ volatile("cpsie i" : : : "memory");  // Enable interrupts
+    ENABLE_IRQ();
 
     while (1)
     {
         uint32_t now;
         // Atomically read current time
-        __asm__ volatile("cpsid i" : : : "memory");
+        DISABLE_IRQ();
         now = g_ms_ticks;
-        __asm__ volatile("cpsie i" : : : "memory");
+        ENABLE_IRQ();
 
         if ((now - start) >= ms)
             break;
+#ifdef __arm__
         __asm__ volatile("nop");
+#endif
     }
 }
 
 uint32_t GetMsTicks()
 {
     uint32_t ticks;
-    __asm__ volatile("cpsid i" : : : "memory");
+    DISABLE_IRQ();
     ticks = g_ms_ticks;
-    __asm__ volatile("cpsie i" : : : "memory");
+    ENABLE_IRQ();
     return ticks;
 }
 
