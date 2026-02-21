35a9b0bec17dff9147d40028bca515fb1870bb66
Bex Saw <bex.bcc171@gmail.com>
2026-02-20 17:29:01 -0800

    rebase protobuff branch to origin/main

---
+++ common/core/protobuff/proto.c
@@ -0,0 +1,28 @@
+#include "protobuff.pb.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+#include <stdio.h>
+
+
+
+int main()
+{
+    uint8_t buffer[128];
+
+    RocketStructProto rocket = RocketStructProto_init_zero;
+
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+    strcpy(rocket.Name, "ROCKET");
+    rocket.Year = 2025;
+    strcpy(rocket.clubName, "LBR");
+    rocket.memberCount = 1;
+
+    bool status = pb_encode(&stream, RocketStructProto_fields, &rocket);
+    // bool read = pb_read(&stream, buffer, sizeof(buffer));
+    if (status)
+    {
+        printf("%s", buffer);
+    }
+
+}
