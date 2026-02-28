d59803d84e26f33e70c7b9985fd79a203132eb0d
Kent Hong <kent.n.hong@gmail.com>
2026-02-28 12:26:50 -0800

    LED pattern works

--- app/blink/main.cc
+++ app/blink/main.cc
@@ -1,7 +1,9 @@
+// TODO: Replace busy-waits with timer interrupts
 #include <array>
 #include <cstddef>
 #include <functional>
 #include "board.h"
+#include "delay.h"
 
 using namespace LBR;
 
@@ -10,31 +12,35 @@ int main(int argc, char* argv[])
     board_init();
     Board& hw = get_board();
 
+#ifdef STM32H723xx
+    std::array<std::reference_wrapper<Gpio>, 3> leds{
+        std::ref(hw.led1), std::ref(hw.led2), std::ref(hw.led3)};
+    leds[0].get().set(1);
+
+#endif
+
     while (1)
     {
 #ifdef STM32H723xx
-        std::array<std::reference_wrapper<Gpio>, 3> leds{
-            std::ref(hw.led1), std::ref(hw.led2), std::ref(hw.led3)};
-
         // Forward
-        for (auto& led : leds)
+        for (size_t i = 0; i < leds.size() - 1; i++)
         {
-            led.get().set(1);
-            for (volatile size_t i = 0; i < 100000; i++);
-            led.get().set(0);
+            for (volatile size_t j = 0; j < 800000; j++);
+            leds[i].get().set(0);
+            leds[i + 1].get().set(1);
         }
 
         // Backward
-        for (int i = leds.size() - 2; i > 0; i--)
+        for (int i = leds.size() - 1; i > 0; i--)
         {
-            leds[i].get().set(1);
-            for (volatile size_t i = 0; i < 100000; i++);
+            for (volatile size_t j = 0; j < 800000; j++);
             leds[i].get().set(0);
+            leds[i - 1].get().set(1);
         }
 #endif
 #ifdef STM32L476xx
         hw.led1.toggle();
-        for (volatile size_t i = 0; i < 100000; i++);
+        for (volatile size_t i = 0; i < 300000; i++);
 #endif
     }
 
