037a86a1489f72c3675c51c15cd91d8f127a8fd7
Alex Pulido <alexp3448@gmail.com>
2026-04-07 20:47:00 -0700

    clang-format bsp

--- app/spi_test/BSP_H723/spi_app_bsp.cc
+++ app/spi_test/BSP_H723/spi_app_bsp.cc
@@ -12,18 +12,20 @@ namespace LBR
 // Make Chip Select Settings
 StGpioSettings cs_gpio_settings{GpioMode::GPOUT, GpioOtype::PUSH_PULL,
                                 GpioOspeed::VERY_HIGH, GpioPupd::NO_PULL, 0};
-StGpioParams cs_params{cs_gpio_settings, 10, GPIOG};   // PG10
+StGpioParams cs_params{cs_gpio_settings, 10, GPIOG};  // PG10
 HwGpio cs_gpio{cs_params};
 
 // Make SPI Register Config Settings
-Stmh7::StSpiSettings spi_settings{Stmh7::SpiBaudRate::FPCLK_8, Stmh7::SpiBusMode::MODE1,
-                                  Stmh7::SpiBitOrder::MSB, Stmh7::SpiDataSize::DSIZE8,
-                                  Stmh7::SpiRxThreshold::FIFO_8};
+Stmh7::StSpiSettings spi_settings{
+    Stmh7::SpiBaudRate::FPCLK_8, Stmh7::SpiBusMode::MODE1,
+    Stmh7::SpiBitOrder::MSB, Stmh7::SpiDataSize::DSIZE8,
+    Stmh7::SpiRxThreshold::FIFO_8};
 Stmh7::HwSpi spi1{SPI1, spi_settings};
 
 // Make GPIO Register Config Settings
-Stmh7::StGpioSettings gpio_settings{Stmh7::GpioMode::ALT_FUNC, Stmh7::GpioOtype::PUSH_PULL,
-                                    Stmh7::GpioOspeed::VERY_HIGH, Stmh7::GpioPupd::NO_PULL, 5};
+Stmh7::StGpioSettings gpio_settings{
+    Stmh7::GpioMode::ALT_FUNC, Stmh7::GpioOtype::PUSH_PULL,
+    Stmh7::GpioOspeed::VERY_HIGH, Stmh7::GpioPupd::NO_PULL, 5};
 Stmh7::StGpioParams sck_params{gpio_settings, 3, GPIOB};
 Stmh7::StGpioParams miso_params{gpio_settings, 9, GPIOG};
 Stmh7::StGpioParams mosi_params{gpio_settings, 7, GPIOD};
@@ -45,7 +47,8 @@ bool bsp_init()
     bool result = true;
 
     // Enable GPIOB, GPIOD, GPIOG clocks
-    RCC->AHB4ENR |= (RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIODEN | RCC_AHB4ENR_GPIOGEN);
+    RCC->AHB4ENR |=
+        (RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIODEN | RCC_AHB4ENR_GPIOGEN);
 
     // Enable the SPI clock
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
@@ -69,4 +72,4 @@ Board& get_board()
 {
     return board;
 }
-}   // namespace LBR
+}  // namespace LBR
