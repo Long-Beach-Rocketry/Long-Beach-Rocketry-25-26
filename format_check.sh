996a2f969f5f1fe7532e9dad74f2deb2a1ed05cb
Bex Saw <bex.bcc171@gmail.com>
2026-04-19 17:58:02 -0700

    i/cd fix

--- format_check.sh
+++ format_check.sh
@@ -1,2 +1,3 @@
 # This script verifies the formatting in the target directories.
-find app common -iname '*.h' -o -iname '*.c' -o -iname '*.cc' | xargs clang-format --dry-run -Werror
+find app common -iname '*.h' -o -iname '*.c' -o -iname '*.cc' | xargs clang-format --dry-run -Werror |
+    grep -v 'common/core/protobuff/generated/'
