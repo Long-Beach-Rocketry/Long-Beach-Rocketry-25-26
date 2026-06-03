bf0a7a3efe8f3bc98d2ee8ec856474b7dc6a655a
Kent Hong <kent.n.hong@gmail.com>
2026-06-02 18:33:53 -0600

    Updated serial prints to output CPU freq and changed format to MHz instead of Hz

--- app/sysclk_test/main.cc
+++ app/sysclk_test/main.cc
@@ -15,25 +15,28 @@ int main(int argc, char** argv)
     while (1)
     {
         const ClockFrequencies& freqs = board.clock.get_clock_frequencies();
+        constexpr uint32_t kHzPerMHz = 1'000'000U;
 
         char buf[320];
         int len = snprintf(buf, sizeof(buf),
                            "--------------------------------\r\n"
                            " SYSCLK TEST\r\n"
                            "--------------------------------\r\n"
-                           " SYSCLK : %10u Hz\r\n"
-                           " HCLK   : %10u Hz\r\n"
-                           " PCLK1  : %10u Hz\r\n"
-                           " PCLK2  : %10u Hz\r\n"
-                           " PCLK3  : %10u Hz\r\n"
-                           " PCLK4  : %10u Hz\r\n"
+                           " SYSCLK : %10u MHz\r\n"
+                           " CPU    : %10u MHz\r\n"
+                           " HCLK   : %10u MHz\r\n"
+                           " PCLK1  : %10u MHz\r\n"
+                           " PCLK2  : %10u MHz\r\n"
+                           " PCLK3  : %10u MHz\r\n"
+                           " PCLK4  : %10u MHz\r\n"
                            "--------------------------------\r\n",
-                           static_cast<unsigned int>(freqs.sysclk),
-                           static_cast<unsigned int>(freqs.ahb),
-                           static_cast<unsigned int>(freqs.apb1),
-                           static_cast<unsigned int>(freqs.apb2),
-                           static_cast<unsigned int>(freqs.apb3),
-                           static_cast<unsigned int>(freqs.apb4));
+                           static_cast<unsigned int>(freqs.sysclk / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.cpu / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.ahb / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.apb1 / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.apb2 / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.apb3 / kHzPerMHz),
+                           static_cast<unsigned int>(freqs.apb4 / kHzPerMHz));
 
         if (len < 0)
         {
