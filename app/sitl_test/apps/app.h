71f35aa605fe470ba2a41e8e461573e4e5afdddf
mariocruz9456 <mario.david.cruz01@gmail.com>
2026-06-13 19:29:51 -0700

    Applying clang format

--- app/sitl_test/apps/app.h
+++ app/sitl_test/apps/app.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // Sensor data which'll be used in every application (airbrake, camera, telemtry)
-struct SensorData {
+struct SensorData
+{
     double time_s;
     double altitude_m;
     double velocity_mps;
@@ -9,9 +10,12 @@ struct SensorData {
 };
 
 // Abstract base class other apps will inherit from
-class App {
-    public:
-        virtual void init() = 0; // Called once at the start of simulation
-        virtual void update(const SensorData& data) = 0; // Called every simulation loop with the latest sensor data
-        virtual ~App() = default; // Virtual destructor for proper cleanup
+class App
+{
+public:
+    virtual void init() = 0;  // Called once at the start of simulation
+    virtual void update(
+        const SensorData&
+            data) = 0;  // Called every simulation loop with the latest sensor data
+    virtual ~App() = default;  // Virtual destructor for proper cleanup
 };
--- app/sitl_test/sim/airbrake_command.h
+++ app/sitl_test/sim/airbrake_command.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // Represents the state of the airbrakes (retracted or deployed)
-struct AirbrakeState {
+struct AirbrakeState
+{
     bool deployment = false;
     // false = retracted
     // true = deployed
