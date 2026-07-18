95165c01a9ea6bd9194f9bdfdf5b11e4f0419ee6
Bex Saw <bex.bcc171@gmail.com>
2026-07-17 19:16:03 -0700

    rebase origin/main

--- common/drivers/platform/stm32h7/st_timebase.h
+++ common/drivers/platform/stm32h7/st_timebase.h
@@ -4,18 +4,9 @@
 * @author Bex Saw
 */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <chrono>
 #include <cstdint>
 #include "stm32h7xx.h"
-=======
->>>>>>> bea3493 (skeleton for timebase)
-=======
-#include <chrono>
-#include <cstdint>
-#include "stm32h7xx.h"
->>>>>>> baa2ac0 (timebase init; stop() & start())
 #include "timebase.h"
 
 namespace LBR
@@ -23,10 +14,6 @@ namespace LBR
 namespace Stmh7
 {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> baa2ac0 (timebase init; stop() & start())
 /**
 * @brief The StTimebaseParams struct contains the parameters needed to initialize the hardware timebase.
 * @param base_addr The base address of the timer peripheral (e.g., TIM2, TIM3, etc.)
@@ -41,18 +28,10 @@ struct StTimebaseParams
     uint32_t timer_input_hz;
     bool enable_irq;
 };
-<<<<<<< HEAD
-=======
-    // enum definition for all of the timebase specific
->>>>>>> bea3493 (skeleton for timebase)
-=======
->>>>>>> baa2ac0 (timebase init; stop() & start())
 
 class HwTimebase : public Timebase
 {
 public:
-<<<<<<< HEAD
-<<<<<<< HEAD
     explicit HwTimebase(const StTimebaseParams& params);
 
     /**
@@ -116,82 +95,3 @@ private:
 };
 }  // namespace Stmh7
 }  // namespace LBR
-=======
-    // explicit constructor 
-=======
-    explicit HwTimebase(const StTimebaseParams& params);
->>>>>>> baa2ac0 (timebase init; stop() & start())
-
-    /**
-    * @brief init the timebase with a specified tick period
-    * @param tick_period The period of the timebase ticks in microseconds
-    */
-    bool init(std::chrono::microseconds tick_period) override;
-
-    /**
-    * @brief start the timebase
-    */
-    void start() override;
-
-    /**
-    * @brief stop the timebase
-    */
-    void stop() override;
-
-    /**
-    * @brief get the current time in microseconds
-    * @return The current time in microseconds
-    */
-    uint64_t now_us() const override;
-
-    /**
-    * @brief get the elapsed time in microseconds since a given time
-    * @param since The starting time in microseconds
-    * @return The elapsed time in microseconds
-    */
-    uint32_t elapsed_us(uint32_t since) const override;
-
-    /**
-    * @brief Set the Frequency of the TIM
-    * @param new_timer_freq The desired new timer frequency
-    * @param pclk The peripheral clock frequency in Hz
-    * @return true if the timer frequency was set successfully, false otherwise
-    */
-    bool set_freq(uint32_t new_timer_freq, uint32_t pclk) override;
-
-    /**
-    * @brief Set the period of the desired timebase TIM channel
-    * @param period The desired period in microseconds
-    * @return true if the period was set successfully, false otherwise
-    */
-    bool set_period(std::chrono::microseconds period) override;
-
-private:
-    /**
-    * @note TIM & IRQ for the timebase constructor init.
-    */
-    TIM_TypeDef* base_addr;
-    IRQn_Type irq;
-
-<<<<<<< HEAD
-    // more variables coming up 
-    TIM_TypeDef* base_addr; 
-}
-}
-}
->>>>>>> bea3493 (skeleton for timebase)
-=======
-    /**
-    * @timer_input_hz: The input frequency of the timer in Hz. 
-    *                  This is used to calculate the timer period for the desired tick period.
-    * @enable_irq: Whether to enable the timer interrupt. If true, the timer interrupt will be 
-    *              enabled and the overflow count will be incremented on each overflow.
-    * @overflow_count: A volatile counter that keeps track of the number of times the timer has overflowed.
-    */
-    uint32_t timer_input_hz;
-    bool enable_irq;
-    volatile uint64_t overflow_count;
-};
-}  // namespace Stmh7
-}  // namespace LBR
->>>>>>> baa2ac0 (timebase init; stop() & start())
