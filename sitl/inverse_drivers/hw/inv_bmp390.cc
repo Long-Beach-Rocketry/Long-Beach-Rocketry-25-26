d1fbbaf3dbb3e3b1f37d43cf01a9c0cc432b0d4b
ioseph01 <j0ni0n.clang@gmail.com>
2026-08-08 21:52:31 -0700

    Inverse bmp390 starter code

--- sitl/inverse_drivers/hw/inv_bmp390.cc
+++ sitl/inverse_drivers/hw/inv_bmp390.cc
@@ -2,18 +2,60 @@
 
 namespace LBR
 {
-bool InvBmp390::process_read(uint8_t opcode, const uint8_t reg_addr,
-                             uint8_t dev_addr)
+
+InvBmp390::InvBmp390(uint8_t dev_addr)
+    : dev_addr_{dev_addr}, recent_reg_{Bmp390_CHIP_ID_REG}
 {
-    (void)opcode;
-    (void)reg_addr;
-    (void)dev_addr;
+}
+
+bool InvBmp390::process_read(const uint8_t reg_addr, uint8_t& data)
+{
+    // Big switch case tree again
+    switch (reg_addr)
+    {
+        case Bmp390_CHIP_ID_REG:
+            data = CHIP_ID;
+            return true;
+
+        default:
+            return false;
+    }
+
     return false;
+}
+
+bool InvBmp390::process_read(uint8_t& data)
+{
+    return process_read(recent_reg_, data);
+}
+
+bool InvBmp390::process_write(const uint8_t reg_addr, uint8_t data)
+{
+    // Another big switch case tree but for write.
+    // TODO: Finish this.
+
+    switch (reg_addr)
+    {
+        /* For example, Bno055 PWR_REG is 0x3E */
+        case Bmp390_PWR_CTRL_REG:
+            // This would be a global variable instead simulating a register.
+            uint8_t Bno055_PWR_REG = data;
+            return true;
+
+        default:
+            return false;
+    }
+    return false;
+}
+
+bool InvBmp390::process_write(uint8_t data)
+{
+    return process_write(recent_reg_, data);
 }
 
 bool InvBmp390::get_8bit_addr(uint8_t& addr)
 {
-    addr = CHIP_ID;
+    addr = dev_addr_;
     return true;
 }
-}
+}  // namespace LBR
