dc4da42a02130d9a694da258631b0b18108185e6
ioseph01 <j0ni0n.clang@gmail.com>
2026-08-08 21:52:41 -0700

    Forgot add .h

--- sitl/inverse_drivers/hw/inv_bmp390.h
+++ sitl/inverse_drivers/hw/inv_bmp390.h
@@ -9,19 +9,29 @@
 namespace LBR
 {
 
+// Would be nice if I could use the values directly from bmp390.h/cc
+// ^ Single source of truth
+constexpr uint8_t Bmp390_CHIP_ID_REG = 0x00;
+constexpr uint8_t Bmp390_PWR_CTRL_REG = 0x1B;
+
 class InvBmp390 : public InvI2cSensor
 {
 public:
-    InvBmp390() = default;
-    ~InvBmp390() = default;
+    explicit InvBmp390(uint8_t dev_addr);
 
     bool process_read(const uint8_t reg_addr, uint8_t& data) override;
 
+    bool process_read(uint8_t& data) override;
+
     bool process_write(const uint8_t reg_addr, uint8_t data) override;
+
+    bool process_write(uint8_t data) override;
 
     bool get_8bit_addr(uint8_t& addr) override;
 
 private:
     static constexpr uint8_t CHIP_ID = 0x60U;
+    uint8_t dev_addr_;
+    uint8_t recent_reg_{Bno055_CHIP_ID_REG};
 };
 }  // namespace LBR
