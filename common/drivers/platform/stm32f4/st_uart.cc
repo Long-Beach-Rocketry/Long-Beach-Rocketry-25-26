bd3362adaf7e52ab9d71b76e7dfebd5de6d065fe
HaiPhan285 <189835136+HaiPhan285@users.noreply.github.com>
2026-09-16 14:23:50 -0700

    hhsh

---
+++ common/drivers/platform/stm32f4/st_uart.cc
@@ -0,0 +1,27 @@
+/*
+*This file implements hardware-specific USART functions.
+*/
+
+//“Do not copy this header more than once.”
+#pragma once 
+#include "stm32f4xx.h"
+#include "st_usart.h"
+
+
+// find 
+namespace LBR{
+    namespace Stmf4{
+        enum class overSamepleMode{
+            BY_16,
+            BY_8
+        };
+        struct st_uart
+        {
+            /* data */
+            
+        };
+        
+    }
+}
+
+
